292b7f16256a349476a9863134ed077399e4e1f2
NeonSkye <marius-david.delcuiescu@s.unibuc.ro>
2022-11-07 03:16:59 +0200

    added the foundation for rendering pixels + some console functions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include<utility>
 //#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include<SFML/System.hpp>
 
 /*vx, vy affected by pixel weight*/
 
@@ -17,79 +18,85 @@ PIXEL TYPES
 4. STATIC SOLID
 5. MOVING SOLID (MAYBE)
 */
+/*PIXEL IDS (so far)
+0. NOTHING
+1. WALL
+2. DUST*/
 
 //weight doesn't matter for static solids (is 0?)
 //type dictates how pixels move
 //density affects how pixels layer (e.g a liquid/powder pixel with density of 2 will go under a liquid/powder pixel with density of 1)
 //temp_init is the initial temperature of the pixel upon spawn (celsius)
-class PixelBehaviour {
-    private:
-    int type;
-    float weight;
-    int vx, vy;
-    int temp;
-    int gravity;
 
-    public:
-    //PixelBehaviour(int type_, int weight_, int temp_, int gravity_) : type{type_}, weight{weight_}, temp{temp_}, gravity{gravity_}
-    PixelBehaviour(int type_, float weight_, int temp_) : type{type_}, weight{weight_}, temp{temp_}
-    {
-        std::cout<<"Constructor Initializare PixelBehaviour"<<std::endl;
-        //std::cout<<"Type "<<type<<std::endl;
-    }
-    friend std::ostream& operator<<(std::ostream& os, const PixelBehaviour& pxb)
-    {
-        os<<"Type PB " << pxb.type <<"\n";
-        return os;
-    }
-    void SetPixel() {}
-    void MovePixel() {}
-    void TransformPixel() {}
+//TO DO: FIGURE OUT HOW TO TRANSLATE MATRIX TO GRAPHICS
+//SIMULATION ENGINE AFTER
 
-
-};
 class Pixel {
     private:
     std::string Name;
+    //sf::Color colour(0,0,0,255);
     int ID;
     int Type;
-    float Weight;
+    int posX, posY;
+    //int x, y;
+    /*float Weight;
     int Density;
     int Temp_init;
     int colour; //change to sfml colour 
     bool Can_burn;
     bool Can_melt;
-    bool Can_ignite;
-    PixelBehaviour PB{this->Type, this->Weight, this->Temp_init};
+    bool Can_ignite; */
+    //PixelBehaviour PB{this->Type, this->Weight, this->Temp_init};
     public:
     // to add PixelBehaviour to Pixel constructor
-    Pixel(std::string Name_, int ID_, int Type_) : Name{Name_}, ID{ID_}, Type{Type_}{
-        std::cout<<"Constructor initializare Pixel"<<std::endl;
-        //std::cout<<"Created Pixel "<< Name <<" With ID "<< ID << " Type " << Type << " And Behaviour " << std::endl;
+/*     int Color(int r, int g, int b, int a)
+    {
+        &colour.r=r;
+        &colour.g=g;
+        &colour.b=b;
+        &colour.a=a;
+    } */
+    Pixel(const std::string Name_, int ID_, int Type_) : Name{Name_}, ID{ID_}, Type{Type_}{
+        /* std::cout<<"Constructor initializare Pixel"<<std::endl;
+        std::cout<<"Created Pixel "<< Name <<" With ID "<< ID << " Type " << Type << " And Behaviour " << std::endl; */
     }
     Pixel(const Pixel& other): Name{other.Name}, ID{other.ID}, Type{other.Type} {
-        std::cout<<"Constructor copiere Pixel"<<std::endl;
+        /* std::cout<<"Constructor copiere Pixel"<<std::endl; */
          //std::cout<<"Copied Pixel "<< Name <<" With ID "<< ID << " Type " << Type << " And Behaviour " << std::endl;
     }
     Pixel& operator=(const Pixel& other) {
         Name = other.Name;
         ID = other.ID;
         Type = other.Type;
-        std::cout<<"Operator= copiere Pixel"<<std::endl;
+        /* std::cout<<"Operator= copiere Pixel"<<std::endl; */
         return *this;
     }
-    /* int getID()
-
+    int getID()
     {
         return ID;
     }
     int getType()
     {
         return Type;
-    } */
+    }
+
+    void setCoords(int X, int Y)
+    {
+       posX = X;
+       posY = Y;
+    }
+    int getposX()
+    {
+        return posX;
+    }
+    int getposY()
+    {
+        return posY;
+    }
+    
     ~Pixel()
     {
-        std::cout <<"Destructor Pixel"<<std::endl;
+        /* std::cout <<"Destructor Pixel"<<std::endl; */
     }
     friend std::ostream& operator<<(std::ostream& os, const Pixel& px){
         os << "Type " << px.Type << " ID " << px.ID << "\n";
@@ -101,94 +108,138 @@ class Pixel {
 
     private:
     std::vector<std::vector<Pixel>> box;
+    sf::Vector2f v1; //Convert box to SFML vector so I can draw the pixels
     int SizeX;
     int SizeY;
     bool has_Border;
-    int Gravity;
-
-    void Create(int SizeX, int SizeY)
+    //int Gravity;
+    public:
+    void Create(int SizeX, int SizeY, bool hasBorder)
     {  
         //std::cout<<"Create World "<<SizeX <<" x "<<SizeY<<std::endl; 
         this->SizeX=SizeX;
         this->SizeY=SizeY;
-        /*for(int i = 0; i<SizeY; i++){
+
+        for(int i = 0; i<SizeY; i++){
             std::vector<Pixel> v1;
 
             for(int j = 0; j<SizeX; j++)
             {
-                v1.push_back(0);
+                if(hasBorder==1)
+                {
+                //std::cout<<"test1";
+                if(i==0||i==SizeY-1)
+                v1.push_back({"WALL",1,4});
+                else
+                if(j==0||j==SizeX-1)
+                v1.push_back({"WALL",1,4});
+                else
+                v1.push_back({"VOID",0,0});
+                }
+                else
+                v1.push_back({"VOID",0,0});
+            }
+            box.push_back(v1); 
+        } 
+    }
+    World(int X_, int Y_, bool hasBorder) {
+        Create(X_,Y_,hasBorder);
+        }
+    
+    
+    void setBox(std::vector<std::vector<Pixel>> box_)
+    {
+        for(int i = 0; i<SizeY; i++){
+            std::vector<Pixel> v1;
+
+            for(int j = 0; j<SizeX; j++)
+            {
+                v1.push_back(box_[i][j]);
             }
             this->box.push_back(v1); 
-        } */
+        } 
     }
-    public:
-    World(int X_, int Y_) {
-        std::cout<<"Constructor initializare World"<<std::endl; Create(X_,Y_);
-        }
-/*     void getWorld()
+
+
+
+    void drawBox()
     {
         
                 std::cout<<"------------------------"<<std::endl;
-        for(int i = 0; i<box.size(); i++)
+        for(int i = 0; i<SizeY; i++)
         {
-            for(int j = 0; j< box[i].size(); j++)
+            for(int j = 0; j<SizeX; j++)
             {
-               std::cout<<box[i][j]<<" ";
+               std::cout<<box[i][j].getID()<<" ";
             }
             std::cout<<std::endl;
         }
         
                 std::cout<<"------------------------"<<std::endl;
     }
-    void SetPixel(int ID, int posX, int posY, int Type)
-    {   
-       
-        box[posY][posX] = Type;
-        if(Type!=4 && Type!=0)
-        {   
-            //IMPROVE EDGE DETECTION
-            
-            getWorld();
-            if(posY<=SizeY-1){
-            MovePixel(ID, posX, posY, Type);
-            }
-           
-            
-            
-        }
-         
-    }
-/*     void MovePixel(int ID, int posX, int posY, int Type)
+    std::vector<std::vector<Pixel>> getBox()
     {
-
-        if(Type==1) //POWDER
-        {
-            // ADD posX and formula with VX
-            if(posY<SizeY-1){
-            posY = posY + 1;  //ADD vy formula
-        
-            
-            SetPixel(ID, posX, posY-1, 0);
-            SetPixel(ID, posX, posY, Type);
-            }
-            else
-            if(posY==SizeY-1)
-            {        
-            SetPixel(ID, posX, posY, Type);
-            }
-            
-            
-        }
-    }    */
+        return box;
+    }
+    /* int getGravity()
+    {
+        return Gravity;
+    } */
+    int getSizeX()
+    {
+        return SizeX;
+    }
+    int getSizeY()
+    {
+        return SizeY;
+    }
     friend std::ostream& operator<<(std::ostream& os, const World& w)
     {
         os<<"Size World " << w.SizeX << " x " <<w.SizeY <<"\n";
         return os;
     }
+        void SetPixel(Pixel P, int posX, int posY)
+    {   
+        box[posX][posY] = P;
+        //std::cout<<box[posX][posY+1].getID()<<"deubg" << std::endl;
+        P.setCoords(posX,posY);
+        //std::cout<< P.getposX() << P.getposY() << "debug2" << std::endl;
+       /*  v1.x = posX;
+        v1.y = posY; */
+    }
+    
+        void movePixel (Pixel P)
+        {
+            std::cout<<"test GetPos"<< P.getposX() << P.getposY() << std::endl;
+            // Din ceva motiv getposX si getposY returneaza valori aiurea si imi crapa toata functia...
+            // In functia SetPixel getposX si getposY returneaza valori normale, adica 2,2 (coordonatele setate in main momentan)
+            // honestly am scris ce e aici la 3 dimineata dupa ce am baut asa ca ¯\_(ツ)_/¯ (●'◡'●)
+            int posX = P.getposX();
+            int posY = P.getposY();
+            if(P.getType()==1)
+            {
+                std::cout<<"getType success"<< posX << posY << std::endl;
+                if(box[posX][posY].getID() == 2)
+                {
+                std::cout<<"checkBox success"<<std::endl;
+                box[posX][posY+1] = P;
+                box[posX][posY] = {"VOID",0,0};
+                P.setCoords(posX,posY+1);
+                }
+                else std::cout<<"checkBox failed"<<std::endl;
+
+
+            }
+
+        } 
+            
+            
+            
+
 };
 
 // buttons for elements
-class Elem_Button{
+/* class Elem_Button{
     private:
     std::string name;
     int ID;
@@ -202,7 +253,7 @@ class Elem_Button{
     Elem_Button(const Pixel& content_) : content{content_} {
         //this->content.ID=content_.getID();
         //this->content.type=content_getType();
-        std::cout<<"Constructor initializare Elem_Button"<<std::endl;
+        // std::cout<<"Constructor initializare Elem_Button"<<std::endl; 
     }
     friend std::ostream& operator<<(std::ostream& os, const Elem_Button& elem_b)
     {
@@ -213,49 +264,126 @@ class Elem_Button{
     void onRightClick() {}// Set RMB to spawn selected box' pixel
     void onMiddleClick() {}// Set MMB to spawn selected box' pixel
 
-};
+}; */
+
+/* class Simulation {
+   private:
+     int vx, vy;
+    int temp;
+    int gravity;
+    Pixel pixel;
+    std::vector<std::vector<Pixel>> box; 
+ 
+    public:
+    //PixelBehaviour(int type_, int weight_, int temp_, int gravity_) : type{type_}, weight{weight_}, temp{temp_}, gravity{gravity_}
+
+  PixelBehaviour(Pixel P_) : pixel{P_}{}
+    friend std::ostream& operator<<(std::ostream& os, const PixelBehaviour& pxb)
+    {
+        os<<"Type PB " << pxb.pixel << "\n";
+        return os;
+    }
+
+
+    void TransformPixel() {}
+
+
+}; */
 
 
 
 int main()
 {
     //sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-    World w1{5,5};
+    World w1{5,5,1};
     std::cout<<w1;
-    Pixel p1{"FIRST",1,2};
-    std::cout<<p1;
+    Pixel p1{"DUST",2,1};
+    /* PixelBehaviour pb1{p1}; */
+    bool isActive = 0;
+    w1.drawBox();
+    /* std::cout<<w1;      Pixel p1{"FIRST",1,2};
+    Pixel p6{"SECOND",2,3};
+     std::cout<<p1; 
     PixelBehaviour pb1{1,2,1};
-    std::cout<<pb1;
+     std::cout<<pb1; 
     Pixel p2{p1};
     Pixel p3 = p1;
     p2 = p3;
-    Elem_Button e1{p1};
-    std::cout<<e1;
-    std::cout<<"6-------------6"<<std::endl;
+    Elem_Button e1{p1}; 
+    std::cout<<e1; 
+     std::cout<<"6-------------6"<<std::endl; */
     //w1.SetPixel(p1.getID(),0,0,p1.getType());
     //w1.getWorld();
     //w1.getWorld();
    // w1.SpawnPixel(TYPE,posX,posY,LOC);
    // w1.Update();
-    return 0;
-// Create the main window
+sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+/* sf::Vector2<Pixel> box; */
+// Limit the framerate to 60 frames per second (this step is optional)
+window.setFramerateLimit(60);
+// The main loop - ends as soon as the window is closed
+sf::RectangleShape rectangle, bg;
+rectangle.setSize(sf::Vector2f(100, 50));
+bg.setSize(sf::Vector2f(800,600));
+bg.setOutlineColor(sf::Color::Black);
+bg.setOutlineThickness(5);
+bg.setFillColor(sf::Color::Black);
+rectangle.setOutlineColor(sf::Color::Red);
+rectangle.setOutlineThickness(5);
+rectangle.setPosition(10, 20);
+window.draw(bg);
+window.draw(rectangle);
+while (window.isOpen())
+{
+   // Event processing
+   window.draw(rectangle);
+   sf::Event event;
+   while (window.pollEvent(event))
+   {
+       // Request for closing the window
+       if (event.type == sf::Event::Closed)
+           window.close();
+   }
 
-    // Load a sprite to displ 
-    // Start the game loop
-  /* while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        // Clear screen
-        window.clear();
-        window.display();
-    }
-    return EXIT_SUCCESS; */
+   // Activate the window for OpenGL rendering
+   window.setActive();
+if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+{
+    w1.SetPixel(p1,2,2);
+    //w1.SetPixel(p1,2,2);
+  
+    
+
+};
+// draw it
+
+
+
+if(isActive == 0) {
+
+if(sf::Mouse::isButtonPressed(sf::Mouse::Right))
+{
+     //w1.movePixel(p1,w1);
+     rectangle.move(10,1);
+     bg.setPosition(0,0);
+     w1.movePixel(p1);
+     //rectangle.setOutlineColor(sf::Color::Blue);
+     isActive = 1;
+     window.draw(rectangle);
+}
+}
+else
+{
+    rectangle.move(0,1);
+    window.draw(bg);
+    window.draw(rectangle);
+    
+}
+
+
+   w1.drawBox();
+   //window.draw(rectangle);
+   window.display();
+}
     
 }
